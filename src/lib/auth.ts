1fc0d94061f0f9ea6296e53863c5ffdd68a22fb1
Quim Crous <joaquimcrous@gmail.com>
2023-05-23 19:16:12 +0200

    test - hoy acabamos aqui XD cuarta parte

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -31,24 +31,24 @@ export const authOptions: NextAuthOptions = {
   },
   jwt: {
     maxAge: 60 * 60 * 24 * 30
+  },
+  callbacks: {
+    async signIn ({ user, account, profile, email, credentials }) {
+      console.log('user', user, account, profile)
+      return true
+    },
+    async redirect ({ url, baseUrl }) {
+      return baseUrl
+    },
+    async session ({ session, token, user }) {
+      return session
+    },
+    async jwt ({ token, user, account, profile, isNewUser }) {
+      // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+      if (account?.accessToken) {
+        token.accessToken = account.accessToken
+      }
+      return token
+    }
   }
-//   callbacks: {
-//     async signIn ({ user, account, profile, email, credentials }) {
-//       console.log('user', user, account, profile)
-//       return true
-//     },
-//     async redirect ({ url, baseUrl }) {
-//       return baseUrl
-//     },
-//     async session ({ session, token, user }) {
-//       return session
-//     },
-//     async jwt ({ token, user, account, profile, isNewUser }) {
-//       // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-//       if (account?.accessToken) {
-//         token.accessToken = account.accessToken
-//       }
-//       return token
-//     }
-//   }
 }
