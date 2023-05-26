47bf6e1bc344f57a88a91368ae80ece492981379
Quim Crous <joaquimcrous@gmail.com>
2023-05-26 13:16:31 +0200

    test quiero probarlo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,28 @@
 'use client'
 import { useSession, signIn, signOut } from 'next-auth/react'
 
+const getApplications = async (session: any) => {
+  const data = await fetch('https://api.infojobs.net/api/5/application', {
+    headers: {
+      Authoritzation: session.accessToken
+    }
+  })
+  const applications = await data.json()
+  return applications
+}
+
 export default function Home () {
   const { data: session } = useSession()
 
   if (session != null) {
+    const applications = getApplications(session)
+    console.log(applications)
+
     return (
       <div>
         <h1>PERO ESTO QUE ES????</h1>
         <h1>estas logueado</h1>
+        <p>{session.user?.name}</p>
         <button onClick={async () => await signOut()}>
           Sign Out
         </button>
