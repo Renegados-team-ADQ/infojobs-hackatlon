a67c8b796977c559245f06175a89ea8975c5beec
Danny <dannym.mejia@gmail.com>
2023-05-29 17:14:33 +0200

    trying things

--- src/app/api/session/route.ts
+++ src/app/api/session/route.ts
@@ -6,14 +6,22 @@ export async function GET (request: Request) {
   const session = await getServerSession(authOptions)
   const basicToken = `Basic ${Buffer.from(`${process.env.INFOJOBS_ID ?? ''}:${process.env.INFOJOBS_SECRET ?? ''}`).toString('base64')}`
   const bearerToken = `Bearer ${session?.accessToken ?? ''}`
-  const data = await fetch('https://api.infojobs.net/api/5/application', {
-    headers: {
-      'Content-type': 'application/json',
-      Authorization: `${basicToken},${bearerToken}`
-    }
-  })
-  const json = await data.json()
-  console.log(json)
+
+  let data
+  let json
+  try {
+    data = await fetch('https://api.infojobs.net/api/5/application', {
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: `${basicToken},${bearerToken}`
+      }
+    })
+    json = await data.json()
+  } catch (error) {
+    console.error('Error fetching data:', error)
+  } finally {
+    console.log(json)
+  }
   return NextResponse.json({
     authenticated: !(session == null),
     session,
