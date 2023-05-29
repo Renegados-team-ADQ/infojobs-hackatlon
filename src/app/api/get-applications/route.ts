d92924799e0130f6f2d8f61d275e339beb9b6e4c
Danny <dannym.mejia@gmail.com>
2023-05-29 17:39:28 +0200

    retrieving data

---
+++ src/app/api/get-applications/route.ts
@@ -0,0 +1,28 @@
+import { getToken } from 'next-auth/jwt'
+import { NextRequest, NextResponse } from 'next/server'
+
+const BASE_URL = 'https://api.infojobs.net'
+const APPLICATION_ENDPOINT = `${BASE_URL}/api/5/application`
+const CLIENT_ID = process.env.INFOJOBS_ID ?? ''
+const CLIENT_SECRET = process.env.INFOJOBS_SECRET ?? ''
+const BASIC_TOKEN = Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString(
+  'base64'
+)
+export async function GET (req: NextRequest) {
+  const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+  if (session == null) {
+    return new NextResponse('Unauthorized', { status: 401 })
+  }
+  const { accessToken } = session
+  const resListCurriculums = await fetch(APPLICATION_ENDPOINT, {
+    headers: {
+      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+      Authorization: `Basic ${BASIC_TOKEN},Bearer ${accessToken}`
+    }
+  })
+  const data = await resListCurriculums.json()
+
+  return NextResponse.json({
+    data
+  })
+}
