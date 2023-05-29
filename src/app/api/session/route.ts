addbbf6273b90780e2f7d9a5e6d2e3a5e646a17d
Danny <dannym.mejia@gmail.com>
2023-05-29 17:27:18 +0200

    json

--- src/app/api/session/route.ts
+++ src/app/api/session/route.ts
@@ -6,22 +6,14 @@ export async function GET (request: Request) {
   const session = await getServerSession(authOptions)
   const basicToken = `Basic ${Buffer.from(`${process.env.INFOJOBS_ID ?? ''}:${process.env.INFOJOBS_SECRET ?? ''}`).toString('base64')}`
   const bearerToken = `Bearer ${session?.accessToken ?? ''}`
-
-  let data
-  let json
-  try {
-    data = await fetch('https://api.infojobs.net/api/5/application', {
-      headers: {
-        'Content-type': 'application/json',
-        Authorization: `${basicToken},${bearerToken}`
-      }
-    })
-    json = await data.json()
-  } catch (error) {
-    console.error('Error fetching data:', error)
-  } finally {
-    console.log(json)
-  }
+  const data = await fetch('https://api.infojobs.net/api/5/application', {
+    headers: {
+      'Content-type': 'application/json',
+      Authorization: `${basicToken},${bearerToken}`
+    }
+  })
+  const json: JSON = await data.json()
+  console.log(json)
   return NextResponse.json({
     authenticated: !(session == null),
     session,
