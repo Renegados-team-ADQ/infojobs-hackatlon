261ed90046552d5f639ed06d86d5f929eb06ebbd
Quim Crous <joaquimcrous@gmail.com>
2023-05-30 18:15:49 +0200

    test - no middleware again

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,6 @@
 import { NextAuthOptions } from 'next-auth'
 
 export const authOptions: NextAuthOptions = {
-  secret: process.env.NEXTAUTH_SECRET ?? '',
   providers: [
     {
       id: 'infojobs',
