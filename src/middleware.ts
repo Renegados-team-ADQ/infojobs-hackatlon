9acc6d247e2110160efbaa6b24ad8f7f84c97507
Quim Crous <joaquimcrous@gmail.com>
2023-05-30 18:12:27 +0200

    test - no middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,2 +1,2 @@
-export { default } from 'next-auth/middleware'
-export const config = { matcher: ['/api/get-applications'] }
+// export { default } from 'next-auth/middleware'
+// export const config = { matcher: ['/api/get-applications'] }
